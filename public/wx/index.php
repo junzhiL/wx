963735f82303b0cb8b0b6966757abe495c757aaa
junzhiL <843334966@qq.com>
2018-11-26 19:47:24 +0800

    jQueryMobile 和 微信静态天气数据

---
+++ public/wx/index.php
@@ -0,0 +1,154 @@
+<?php
+/**
+  * wechat php test
+  */
+
+//define your token
+define("TOKEN", "weather");
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->responseMsg();
+//$wechatObj->valid();
+
+class wechatCallbackapiTest
+{
+    /*public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }*/
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+          //extract post data
+        if (!empty($postStr)){
+                
+                  $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $RX_TYPE = trim($postObj->MsgType);
+
+                switch($RX_TYPE)
+                {
+                    case "text":
+                        $resultStr = $this->handleText($postObj);
+                        break;
+                    case "event":
+                        $resultStr = $this->handleEvent($postObj);
+                        break;
+                    default:
+                        $resultStr = "Unknow msg type: ".$RX_TYPE;
+                        break;
+                }
+                echo $resultStr;
+        }else {
+            echo "";
+            exit;
+        }
+    }
+
+    public function handleText($postObj)
+    {
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $keyword = trim($postObj->Content);
+        $time = time();
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";             
+        if(!empty( $keyword ))
+        {
+            $msgType = "text";
+
+            //天气
+            $str = mb_substr($keyword,-2,2,"UTF-8");
+            $str_key = mb_substr($keyword,0,-2,"UTF-8");
+            if($str == '天气' && !empty($str_key)){
+                $data = $this->weather($str_key);
+                if(empty($data->weatherinfo)){
+                    $contentStr = "抱歉，没有查到\"".$str_key."\"的天气信息！";
+                } else {
+                    $contentStr = "【".$data->weatherinfo->city."天气预报】\n"."2018年11月25日  20时发布"."\n\n实时天气\n"."晴 -3℃～10℃ 南风2级 轻度污染"."\n\n温馨提示："."温度较低，适宜穿风衣，注意做好保暖措施。今日轻度污染，适宜在室内运动"."\n\n明天\n"."晴 -2℃～10℃ 西南风4级 中度污染"."\n\n后天\n"."晴 -3℃～8℃ 北风4级 轻度污染";
+                }
+            } else {
+                $contentStr = "感谢您关注由柳俊志开发的微信天气预报公众号！！！";
+            }
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            echo $resultStr;
+        }else{
+            echo "Input something...";
+        }
+    }
+
+    public function handleEvent($object)
+    {
+        $contentStr = "";
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $contentStr = "感谢您关注由柳俊志开发的微信天气预报公众号！！！"."\n";
+                break;
+            default :
+                $contentStr = "Unknow Event: ".$object->Event;
+                break;
+        }
+        $resultStr = $this->responseText($object, $contentStr);
+        return $resultStr;
+    }
+    
+    public function responseText($object, $content, $flag=0)
+    {
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>%d</FuncFlag>
+                    </xml>";
+        $resultStr = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content, $flag);
+        return $resultStr;
+    }
+
+    private function weather($n){
+        include("weather_cityId.php");
+        $c_name=$weather_cityId[$n];
+        if(!empty($c_name)){
+            $json=file_get_contents("http://www.weather.com.cn/data/sk/".$c_name.".html");
+           return json_decode($json);
+        } else {
+            return null;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];    
+                
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+        
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+?>
